0eab7aca0058b6b2782e27db3098e6bd43b48f42
Santo <matthewalesandro78@gmail.com>
2022-01-19 20:06:51 +0700

    nyoba nyoba

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -1,41 +1,63 @@
-// import React from "react";
-// import { useState,useEffect } from "react";
+import React from "react";
+import { useState,useEffect } from "react";
 
-// import axios from "axios";
+import axios from "axios";
 
-// import {Row,Col,Button,Modal, Container,Card} from "react-bootstrap"
-
-
-// import Menu from "../component/navbarfooter/navbar"
-// import Footer from "../component/navbarfooter/footer"
+import {Row,Col,Button,Modal, Container,Card} from "react-bootstrap"
 
 
-// const Details = () =>{
+import Menu from "../component/navbarfooter/navbar"
+import Footer from "../component/navbarfooter/footer"
 
 
-// //Data
-// const [recipe,setRecipe]= useState([]);
+const Details = () =>{
 
-//         const GetAllRecipe = async()=>{
-//             const response = await axios.get('http://localhost:6999/get')
-//             setRecipe(response.data);
-//         }
 
-//         useEffect(()=>{
-//             GetAllRecipe();
-//         },[])
+//Data
+const [recipe,setRecipe]= useState([]);
 
-//     return(
-//         <>
-//         <Menu/>
 
-//             <Row>
+        const ShowDetails = async(id)=>{
+            const response2 = await axios.get(`http://localhost:6999/get/${id}`)
+            setRecipe(response2.data);
+        }
 
-//             </Row>
 
-//         <Footer/>
-//         </>
-//     )
-// }
+        useEffect(()=>{
+            ShowDetails()
+        },[])
 
-// export default Details
+    return(
+        <>
+
+        <Menu/>
+
+            <Row>
+                <Col>
+
+                    {
+                        recipe.map((row)=>(
+                            <Col key={row.id} lg="3" className="d-flex justify-content-center listresep" >
+                            <Card style={{ width: '18rem' }}>
+                            <Card.Img src={row.img_url} height={"175px"}  cross-origin="anonymous" alt={row.title}/>
+                            <Card.Body>
+                              <Card.Title >{row.title}</Card.Title>
+                              <Card.Subtitle>{row.bahan}</Card.Subtitle>
+                              <Card.Text>
+                                {row.caption}
+                              </Card.Text>
+                            </Card.Body>
+                          </Card>
+                        </Col>
+                        ))
+                    }
+                </Col>
+            </Row>
+
+        <Footer/>
+
+        </>
+    )
+}
+
+export default Details
